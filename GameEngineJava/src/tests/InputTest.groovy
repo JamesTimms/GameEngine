386b3535b564505fbad0cc1fee31bb2479ca1acc
James Timms PC <James-Timms@hotmail.com>
2014-07-14 15:45:44 +0100

    Fixed Groovy annotation bug for Unit Testing and library link issues.

--- GameEngineJava/src/tests/InputTest.groovy
+++ GameEngineJava/src/tests/InputTest.groovy
@@ -1,15 +1,20 @@
 package tests
 
-import org.junit.*
-
 import org.gameEngine.engine.core.Input
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 import org.lwjgl.input.Keyboard
 import org.lwjgl.opengl.Display
 
 /**
  * Created by TekMaTek on 04/07/2014.
  */
-public class InputTest {
+
+@RunWith( JUnit4.class )
+public class InputTest extends groovy.util.GroovyTestCase {
 
     @BeforeClass
     public static void SetupKeyboard() {
@@ -25,19 +30,19 @@ public class InputTest {
 
     @Test
     void testGetKeyAndSucceed() {
-//        Input.InjectKeyPress( 56 )
-//        assertTrue( Input.GetKey( 56 ) )
+        Input.InjectKeyPress( 56 )
+        assertTrue( Input.GetKey( 56 ) )
     }
 
     @Test
     void testGetKeyAndFail() {
-//        Input.InjectKeyPress( 56 )
-//        assertFalse( Input.GetKey( 57 ) )
+        Input.InjectKeyPress( 56 )
+        assertFalse( Input.GetKey( 57 ) )
     }
 
     @Test
     void testGetKeyDown() {
-
+        Input.InjectKeyPress(  )
     }
 
     @Test
--- GameEngineJava/src/tests/unitTestTesting/JunitTest1.groovy
+++ GameEngineJava/src/tests/unitTestTesting/JunitTest1.groovy
@@ -1,16 +1,20 @@
 package unitTesting
 
 import org.junit.*
-
-import static org.junit.Assert.*;
-import java.util.*;
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
 /**
  * @author TekMaTek on 08/07/2014
  * Modified from http://www.mkyong.com/unittest/junit-4-tutorial-1-basic-usage/
+ *
+ * Note: GroovyTestCase extends JUnit (using JUnit 3) which prevents annotations from working
+ * by using @RunWith( JUnit4.class ) the class is forced to be run using JUnit4 instead of 3.
+ * This allows Groovy to be extended and for JUnit 4 annotations to work.
  */
 
-public class JunitTest1 {
+@RunWith( JUnit4.class )
+public class JunitTest1 extends groovy.util.GroovyTestCase {
 
     private Collection collection;
     public static String myString = "";
@@ -30,26 +34,26 @@ public class JunitTest1 {
 
     @Before
     public void setUp() {
-        collection = new ArrayList();
+        collection = new ArrayList( );
         myString += "@Before - setUp\n"
     }
 
     @After
     public void tearDown() {
-        collection.clear();
+        collection.clear( );
         myString += "@After - tearDown\n"
     }
 
     @Test
     public void testEmptyCollection() {
-        assertTrue(collection.isEmpty());
+        assertTrue( collection.isEmpty( ) );
         myString += "@Test - testEmptyCollection\n"
     }
 
     @Test
     public void testOneItemCollection() {
-        collection.add("itemA");
-        assertEquals(1, collection.size());
+        collection.add( "itemA" );
+        assertEquals( 1, collection.size( ) );
         myString += "@Test - testOneItemCollection\n"
     }
 }
