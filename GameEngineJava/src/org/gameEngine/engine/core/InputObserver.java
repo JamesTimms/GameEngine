d9f01732b8d90a1a5df3ec316719977d6df435e4
James Timms PC <James-Timms@hotmail.com>
2014-07-24 19:23:02 +0100

    Working on unit tests still. May try to use inversion of control principles in the design some.

--- GameEngineJava/src/org/gameEngine/engine/core/InputObserver.java
+++ GameEngineJava/src/org/gameEngine/engine/core/InputObserver.java
@@ -14,12 +14,12 @@ public class InputObserver extends Observable {
 	public static final String KEY_PRESSED_EVENT = "KeyPressed";
 	public static final String KEY_RELEASED_EVENT = "KeyReleased";
 
-	private static ArrayList< Integer > keysHeld = new ArrayList< Integer >( );
-	private static ArrayList< Integer > keysPressed = new ArrayList< Integer >( );
-	private static ArrayList< Integer > keysReleased = new ArrayList< Integer >( );
-	private static ArrayList< Integer > injectedKeys = new ArrayList< Integer >( );
+	protected static ArrayList< Integer > keysHeld = new ArrayList< Integer >( );
+	protected static ArrayList< Integer > keysPressed = new ArrayList< Integer >( );
+	protected static ArrayList< Integer > keysReleased = new ArrayList< Integer >( );
+	protected static ArrayList< Integer > injectedKeys = new ArrayList< Integer >( );
 
-	private static int numOfKeyboardEvents;
+	protected static int numOfKeyboardEvents;
 
 	public InputObserver( ) {
 		numOfKeyboardEvents = Keyboard.KEYBOARD_SIZE;
@@ -45,7 +45,7 @@ public class InputObserver extends Observable {
 		}
 	}
 
-	private static void RegisterKeyboardActions( ) {
+	protected static void RegisterKeyboardActions( ) {
 		keysReleased.clear( );
 		for( int i = 0; i < numOfKeyboardEvents; i++ ) {
 			if( !IsKeyPressed( i ) && keysHeld.contains( i ) ) {
@@ -69,15 +69,15 @@ public class InputObserver extends Observable {
 		injectedKeys.clear( );
 	}
 
-	private static boolean IsKeyHeld( int keyNumber ) {
+	protected static boolean IsKeyHeld( int keyNumber ) {
 		return keysPressed.contains( keyNumber );
 	}
 
-	private static boolean IsKeyPressed( int keyNumber ) {
+	protected static boolean IsKeyPressed( int keyNumber ) {
 		return Keyboard.isKeyDown( keyNumber ) || injectedKeys.contains( keyNumber );
 	}
 
-	private static boolean IsKeyReleased( int keyNumber ) {
+	protected static boolean IsKeyReleased( int keyNumber ) {
 		return keysReleased.contains( keyNumber );
 	}
 
