6c2e0e8a498cbd6a08a96656f12c725f423bbcc9
James Timms PC <James-Timms@hotmail.com>
2015-02-01 16:52:27 +0000

    Refactored a little more.

--- GameEngineJava/src/org/gameEngine/Game.java
+++ GameEngineJava/src/org/gameEngine/Game.java
@@ -15,12 +15,16 @@ import org.gameEngine.engine.rendering.shaders.Material;
  */
 public class Game {
 
+	private GameObject root;
 	private GameObject plane;
 	private Camera camera;
 	private double temp;
 
 	public void init( ) {
+		root = new GameObject( );
 		plane = new GameObject( );
+
+		root.addChild( plane );
 		camera = new Camera( );
 		camera.setProjection( 70.0f, StartGame.WIDTH, StartGame.HEIGHT, 0.1f, 1000.0f );
 
@@ -47,6 +51,6 @@ public class Game {
 
 	public void Render( ) {
 		RenderingUtil.setClearColor( new Vector3f( 0.0f, 0.0f, 0.0f ) );
-		plane.render( );
+		root.render( );
 	}
 }
